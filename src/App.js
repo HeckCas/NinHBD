1a9f4f601e1ffe96d532d4c2fc4452c1f8b1edea
Iván Castelán 👾 <gho0thubun@gmail.com>
2022-10-22 04:53:27 -0500

    feat: App non used stuff corrections

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import './App.css';
-import Card from './Card.jsx'
 import Hero from './Hero.jsx'
 import Sql from './Sql.jsx'
 import Term from './Terminal.jsx'
-import Cake from './cake.png';
 function App() {
   return (
     <div>
